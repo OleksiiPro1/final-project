6e0e939e3a7424d0031339e855467811491a990a
Alexey Piltenko <ukr.top@ukr.net>
2022-07-04 18:25:26 +0300

    Works!)

--- pages/cart.js
+++ pages/cart.js
@@ -6,12 +6,19 @@ import { useEffect } from 'react';
 import { getCourses } from '../util/database';
 import VideoComponent from './component/VideoComponentCart';
 
+const allText = css`
+  color: white;
+  list-style-type: none;
+  margin-right: 60px;
+  
+`;
+
 const mainH1 = css`
- z-index: 1;
- position: absolute;
-margin-top: -435px;
-margin-left: 60px;
-color: white;
+  z-index: 1;
+  position: absolute;
+  margin-top: -435px;
+  margin-left: 60px;
+  color: white;
 `;
 const mainVideoDiv = css`
   margin-top: -450px;
@@ -20,52 +27,59 @@ const mainVideoDiv = css`
 `;
 
 export default function CoursesList(props) {
-
-
-
-
   useEffect(() => {
     const currentCart = Cookies.get('cart')
       ? JSON.parse(Cookies.get('cart'))
       : [];
-      console.log(currentCart);
+    console.log(currentCart);
   }, []);
 
-
   return (
-
     <div>
-<Head>
+      <Head>
         <title>Mission React</title>
 
         <meta name="description" content="Mission React - mission-react.com" />
         <link rel="icon" href="/favicon.ico" />
-</Head>
+      </Head>
       <div>
         <Image
           src="/welcome on board2.png"
           width="1549px"
           height="484px"
           alt="courses"
-        /></div>
-<div css={mainVideoDiv}>
+        />
+      </div>
+      <div css={mainVideoDiv}>
         <VideoComponent />
       </div>
-<div  css={mainH1}><h1>Welcome on board!</h1></div>
+      <div css={mainH1}>
+        <h1>Welcome on board!</h1>
+      </div>
 
-     <ul> {props.courses.map((course) => {
-        return<div key={`course-${course.id}`}>
-
-          <li>{course.planet}</li>
-          <li>{course.price}</li>
-          <li>{course.description}</li>
-
-
-          </div>;
-      })}</ul>
+      <ul css={allText}>
+        {' '}
+        {props.courses.map((course) => {
+          return (
+            <div key={`course-${course.id}`}>
+              <li>
+                <Image
+                  src={`/${course.id}.png`}
+                  width="100px"
+                  height="100px"
+                  alt="courses"
+                />
+              </li>
+              <li>Course: {course.planet}</li>
+              <li>Prise: {course.price}$</li>
+              <li>Description: {course.description}</li>
+              <br /><hr /><br />
+            </div>
+          );
+        })}
+      </ul>
 
     </div>
-
   );
 }
 
